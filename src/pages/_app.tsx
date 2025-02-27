b9a402dd239ce0333645cad8431179ae78241f8c
Lucas <lucas.almdiniz@gmail.com>
2025-02-26 21:47:43 -0400

    refactor: Estilos importados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+
+import '@/styles/globals.scss';
+import '@/styles/Login.scss';
+import '@/styles/InputText.scss';
+import '@/styles/Button.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
